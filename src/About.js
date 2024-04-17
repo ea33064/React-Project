e7322f408da25bca74828eb213b7d724106e0363
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-17 09:54:31 +0000

    update about

--- src/About.js
+++ src/About.js
@@ -2,13 +2,39 @@ import React from "react";
 
 const About = () => {
   return (
-    <div id="about">
-      <h1>About</h1>
-      <p>
-        This is a simple todo app built with React. It allows you to add,
-        complete, and delete todos.
-      </p>
-    </div>
+    <section id="about">
+      <div className="row">
+        <div className="col-lg-4 ms-auto">
+          <h2 className="section-heading" style={{ textAlign: "center" }}>
+            About Me
+          </h2>
+          <p className="lead">
+            Hi! Im Eric, a dynamic and driven Software Engineer with a passion
+            for leveraging cutting-edge technologies to develop innovative
+            solutions.
+          </p>
+        </div>
+        <div className="col-lg-4 me-auto">
+          <p className="lead">
+            Eager to contribute my solid foundation in programming languages and
+            software development methodologies to a collaborative team
+            environment. Seeking an opportunity to further enhance my skills and
+            knowledge while making meaningful contributions to impactful
+            projects in the software engineering field.
+          </p>
+        </div>
+      </div>
+      {/* About Section Button*/}
+      <div className="container text-center mt-4">
+        <a
+          className="btn btn-xl btn-outline-light"
+          href="https://github.com/ea33064"
+          style={{ textAlign: "center" }}
+        >
+          View my GitHub!
+        </a>
+      </div>
+    </section>
   );
 };
 
