880963070214eb56d33a49d670f9b50da54a61df
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-17 09:24:03 +0000

    update app.js added imports for errors, paths

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Todos from "./Todos";
+import About from "./About";
 import Contact from "./Contact";
-import "./styles.css";
+import ErrorBoundary from "./ErrorBoundary";
 
-ReactDOM.render(
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <a href="#todos">Todos</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-    <Todos />
-    <Contact />
-  </div>,
-  document.getElementById("root"),
-);
+const App = () => {
+  const routes = [
+    { path: "/todos", element: <Todos /> },
+    { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> },
+  ];
+
+  return (
+    <ErrorBoundary>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="/todos">Todos</a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+        </div>
+      </Router>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
