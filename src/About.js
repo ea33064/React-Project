d8c33acb097a927bc9a1a151491fd8b5e6140940
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-17 09:17:58 +0000

    finish about setup

--- src/About.js
+++ src/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>
+        This is a simple todo app built with React. It allows you to add,
+        complete, and delete todos.
+      </p>
+    </div>
+  );
 };
 
 export default About;
