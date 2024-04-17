75666aafbca45e372b2be9b2d16f3ae557b1f375
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-17 08:03:41 +0000

    todos completed

--- src/Todos.js
+++ src/Todos.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todos = () => {
-  const Todos = ([todos, setTodos] = useState([]));
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: todos.length + 1, text, completed: false }]);
+  };
+
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
+
+  return (
+    <div id="todos">
+      <h1>Todos</h1>
+      <form id="add-todo-form">
+        <input type="text" id="new-todo" placeholder="Add a new task..." />
+        <button
+          type="button"
+          onClick={() => addTodo(document.getElementById("new-todo").value)}
+        >
+          Add
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => toggleCompleted(todo.id)}>
+            {todo.completed ? <strike>{todo.text}</strike> : todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+export default Todos;
