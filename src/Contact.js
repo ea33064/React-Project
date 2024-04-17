6d9c415fe712ff25976c6ed829c1966aa8a0cfb5
Eric <166804530+ea33064@users.noreply.github.com>
2024-04-17 07:20:21 +0000

    complete contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -3,8 +3,18 @@ import React from "react";
 const Contact = () => {
   return (
     <div id="contact">
-      <h1>Contact Page</h1>
-      <form></form>
+      <h1>Contact</h1>
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
+
+export default Contact;
